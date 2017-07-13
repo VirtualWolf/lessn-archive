7ff10a85327598159573e639cb965c485d42ba7f
VirtualWolf <virtualwolf@virtualwolf.org>
2017-07-13 21:12:59 +1000

    Added PG_PORT environment variable because node-pg seems to behave weirdly with it missing.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const pgConfig = {
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT,
 };
 
 const pool = new Pool(pgConfig);
